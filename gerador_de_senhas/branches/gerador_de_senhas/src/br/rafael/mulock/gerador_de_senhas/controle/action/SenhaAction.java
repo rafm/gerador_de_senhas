3446d4f46c300d9636efc9ee5d3f087de05ff26a
nobody <nobody@1849f7dd-3d01-0010-a64c-192886a73bcc>
2013-04-07 20:27:13 +0000

    80% do sistema finalizado. Só falta terminar o SenhaAction.java e a TelaPasso3.java.

--- gerador_de_senhas/branches/gerador_de_senhas/src/br/rafael/mulock/gerador_de_senhas/controle/action/SenhaAction.java
+++ gerador_de_senhas/branches/gerador_de_senhas/src/br/rafael/mulock/gerador_de_senhas/controle/action/SenhaAction.java
@@ -2,25 +2,31 @@ package br.rafael.mulock.gerador_de_senhas.controle.action;
 
 import java.util.Map;
 
-public class SenhaAction {
+public final class SenhaAction {
 
-	public String gerarSenha(Map<String, String> parametros) throws Exception {
-
-		int qtdCaracteres;
+	public static final int LETRAS_MINUSCULAS = 1;
+	public static final int LETRAS_MAIUSCULAS = 2;
+	public static final int SIMBOLOS = 3;
+	
+	public boolean gerarSenha(Map<String, Object> parametros) {
 		
-		try {
-			qtdCaracteres = Integer.parseInt(parametros.get("qtdCaracteres"));
-		} catch (Exception e) {
-			parametros.put("mensagem", "Informe somente números no campo \"Quantidade de caracteres\"");
-			return null;
-		}
+		return true;
 
-		parametros.get("letrasMinusculas");
-		parametros.get("letrasMaiusculas");
-		parametros.get("simbolos");
-		parametros.get("ordemLetrasMinusculas");
-		parametros.get("ordemLetrasMaiusculas");
-		parametros.get("ordemSimbolos");
-		return null;
+//		int qtdCaracteres;
+//		
+//		try {
+//			qtdCaracteres = Integer.parseInt(parametros.get("qtdCaracteres"));
+//		} catch (Exception e) {
+//			parametros.put("mensagem", "Informe somente números no campo \"Quantidade de caracteres\"");
+//			return false;
+//		}
+//
+//		parametros.get("letrasMinusculas");
+//		parametros.get("letrasMaiusculas");
+//		parametros.get("simbolos");
+//		parametros.get("ordemLetrasMinusculas");
+//		parametros.get("ordemLetrasMaiusculas");
+//		parametros.get("ordemSimbolos");
+//		return true;
 	}
 }
--- gerador_de_senhas/branches/gerador_de_senhas/src/br/rafael/mulock/gerador_de_senhas/visao/TelaPrincipal.java
+++ gerador_de_senhas/branches/gerador_de_senhas/src/br/rafael/mulock/gerador_de_senhas/visao/TelaPrincipal.java
@@ -13,26 +13,17 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
+import java.awt.Color;
 
-import br.rafael.mulock.gerador_de_senhas.controle.action.SenhaAction;
-
-public class TelaPrincipal extends JFrame {
+public final class TelaPrincipal extends JFrame {
 
 
 	/**
-	 * Versão 1.0 da parte visual da aplicação
+	 * Primeira tela a ser exibida pela aplicação
 	 */
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
-	private JTextField txtA;
-	private JTextField textField;
-	private JTextField textField_1;
-	private JTextField textField_2;
-	private JTextField textField_3;
-	private JTextField textField_4;
-	private JTextField textField_5;
-	private JTextField textField_6;
-	private SenhaAction senhaAction;
+	private JTextField txtQtdCaracteres;
 
 	/**
 	 * Launch the application.
@@ -56,123 +47,72 @@ public class TelaPrincipal extends JFrame {
 	public TelaPrincipal() {
 		setTitle("Gerador de senhas");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(0, 0, 600, 300);
 		setLocationRelativeTo(null);
-		setBounds(100, 100, 600, 300);
 		contentPane = new JPanel();
+		contentPane.setBackground(Color.WHITE);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		senhaAction = new SenhaAction();
 		
 		JLabel lblQuantidadeDeCaracteres = new JLabel("Quantidade de caracteres:");
-		lblQuantidadeDeCaracteres.setBounds(10, 14, 134, 14);
+		lblQuantidadeDeCaracteres.setBounds(10, 59, 151, 14);
 		contentPane.add(lblQuantidadeDeCaracteres);
 		
-		txtA = new JTextField();
-		txtA.setBounds(176, 11, 97, 20);
-		contentPane.add(txtA);
-		txtA.setColumns(10);
+		txtQtdCaracteres = new JTextField();
+		txtQtdCaracteres.setBounds(171, 56, 97, 20);
+		contentPane.add(txtQtdCaracteres);
+		txtQtdCaracteres.setColumns(10);
 		
-		JLabel lblLetrasMinsculas = new JLabel("Letras min\u00FAsculas:");
-		lblLetrasMinsculas.setBounds(10, 45, 97, 14);
-		contentPane.add(lblLetrasMinsculas);
-		
-		textField = new JTextField();
-		textField.setBounds(176, 42, 97, 20);
-		contentPane.add(textField);
-		textField.setColumns(10);
-		
-		JLabel lblLetrasMaisculas = new JLabel("Letras mai\u00FAsculas:");
-		lblLetrasMaisculas.setBounds(10, 76, 97, 14);
-		contentPane.add(lblLetrasMaisculas);
-		
-		textField_1 = new JTextField();
-		textField_1.setBounds(176, 73, 97, 20);
-		contentPane.add(textField_1);
-		textField_1.setColumns(10);
-		
-		JLabel lblSmboloscaracteresEspeciais = new JLabel("S\u00EDmbolos/Caracteres especiais:");
-		lblSmboloscaracteresEspeciais.setBounds(10, 107, 148, 14);
-		contentPane.add(lblSmboloscaracteresEspeciais);
-		
-		textField_2 = new JTextField();
-		textField_2.setToolTipText("");
-		textField_2.setName("");
-		textField_2.setBounds(176, 104, 97, 20);
-		contentPane.add(textField_2);
-		textField_2.setColumns(10);
-		
-		JLabel lblOrdem = new JLabel("Ordem:");
-		lblOrdem.setBounds(316, 45, 46, 14);
-		contentPane.add(lblOrdem);
-		
-		textField_3 = new JTextField();
-		textField_3.setBounds(363, 42, 97, 20);
-		contentPane.add(textField_3);
-		textField_3.setColumns(10);
-		
-		JLabel lblOrdem_1 = new JLabel("Ordem:");
-		lblOrdem_1.setBounds(316, 76, 46, 14);
-		contentPane.add(lblOrdem_1);
-		
-		textField_4 = new JTextField();
-		textField_4.setBounds(363, 73, 97, 20);
-		contentPane.add(textField_4);
-		textField_4.setColumns(10);
-		
-		JLabel lblOrdem_2 = new JLabel("Ordem:");
-		lblOrdem_2.setBounds(316, 107, 46, 14);
-		contentPane.add(lblOrdem_2);
-		
-		textField_5 = new JTextField();
-		textField_5.setBounds(363, 104, 97, 20);
-		contentPane.add(textField_5);
-		textField_5.setColumns(10);
-		
-		JButton btnGerarSenha = new JButton("Gerar senha");
+		JButton btnGerarSenha = new JButton("Seguinte");
 		btnGerarSenha.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				Map<String, String> parametros = new HashMap<String, String>();
-				parametros.put("qtdCaracteres", txtA.getText());
-				parametros.put("letrasMinusculas", textField.getText());
-				parametros.put("letrasMaiusculas", textField_1.getText());
-				parametros.put("simbolos", textField_2.getText());
-				parametros.put("ordemLetrasMinusculas", textField_3.getText());
-				parametros.put("ordemLetrasMaiusculas", textField_4.getText());
-				parametros.put("ordemSimbolos", textField_5.getText());
-				String senha = null;
-				try {
-					senha = senhaAction.gerarSenha(parametros);
-					if (senha == null) {
-						textField_6.setEnabled(false);
-						textField_6.setVisible(false);
-						textField_6.setText("");
-					} else {
-						textField_6.setEnabled(true);
-						textField_6.setVisible(true);
-						textField_6.setText(senha);
+				
+				if("".equals(txtQtdCaracteres.getText())) {
+					JOptionPane.showMessageDialog(null, "Digite uma quantidade de caracteres");
+				} else {
+					try {
+						int qtdCaracteres = Integer.parseInt(txtQtdCaracteres.getText());
+						
+						if (qtdCaracteres > 0) {
+							Map<String, Object> parametros = new HashMap<String, Object>();
+							parametros.put("qtdCaracteres", qtdCaracteres);
+							
+							dispose();
+							TelaPasso2 telaPasso2 = new TelaPasso2(parametros);
+							telaPasso2.setVisible(true);
+						} else {
+							JOptionPane.showMessageDialog(null, "Digite uma quantidade maior do que 0");
+						}
+					} catch (Exception e) {
+						JOptionPane.showMessageDialog(null, "Digite somente números inteiros");
 					}
-					
-					String mensagem = parametros.get("mensagem");
-					if (mensagem == null)
-						JOptionPane.showMessageDialog(null, "Houve um erro desconhecido.");
-					else
-						JOptionPane.showMessageDialog(null, mensagem);
-					
-				} catch (Exception e) {
-					JOptionPane.showMessageDialog(null, "Houve um erro:\n"+e.getMessage());
 				}
+
+//				parametros.put("letrasMinusculas", txtLetrasMinusculas.getText());
+//				parametros.put("letrasMaiusculas", txtLetrasMaiusculas.getText());
+//				parametros.put("simbolos", txtSimbolos.getText());
+//
+//				try {
+//					if (senhaAction.gerarSenha(parametros)) {
+//						txtSenha.setEnabled(true);
+//						txtSenha.setVisible(true);
+//						txtSenha.setText(parametros.get("senha"));
+//					} else {
+//						txtSenha.setEnabled(false);
+//						txtSenha.setVisible(false);
+//						txtSenha.setText("");
+//					}
+//
+//					if (parametros.get("mensagem") != null)
+//						JOptionPane.showMessageDialog(null, parametros.get("mensagem"));
+//				} catch (Exception e) {
+//					e.printStackTrace();
+//					JOptionPane.showMessageDialog(null, "Houve um erro:\n"+e.getMessage());
+//				}
 			}
 		});
-		btnGerarSenha.setBounds(10, 153, 111, 23);
+		btnGerarSenha.setBounds(304, 55, 111, 23);
 		contentPane.add(btnGerarSenha);
-		
-		textField_6 = new JTextField();
-		textField_6.setEnabled(false);
-		textField_6.setEditable(false);
-		textField_6.setVisible(false);
-		textField_6.setBounds(10, 204, 160, 20);
-		contentPane.add(textField_6);
-		textField_6.setColumns(10);
 	}
 }
